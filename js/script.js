b9f3acec71651f025ff87876eb49b0e06c1ac1e4
Valentina-Fr <v_frasca@virgilio.it>
2021-07-17 13:04:39 +0200

    Add form

--- js/script.js
+++ js/script.js
@@ -11,12 +11,19 @@ const randomNumbers = [];
 //Array numeri indovinati
 const matchingNumbers = [];
 
+//Array numeri utente
+const userNumbers = [];
+
 //Timer
 let seconds = 30;
 
 //Recupero elemento html
 var countdown = document.getElementById("countdown");
-countdown.innerText = seconds;
+var userGuesses = document.getElementById("user-guesses");
+var guess = document.getElementById("number-guess");
+var send = document.getElementById("send");
+var alertMessage = document.getElementById("alert");
+countdown.innerText = seconds--;
 
 //Generare 5 numeri casuali
 while (randomNumbers.length < 5) {
@@ -25,31 +32,50 @@ while (randomNumbers.length < 5) {
         randomNumbers.push(random);
     }
 }
-alert(randomNumbers);
+
+//Stampo numeri casuali
+alert(randomNumbers.join(", "));
 console.table(randomNumbers);
 
 //Parte il timer
 var timer = setInterval (function(){
-    if(seconds === 0) {
+    //Timer si ferma a 0
+    if (seconds === 0) {
         clearInterval(timer);
-        countdown.innerText = "";
-        for (let i = 0; i < 5; i++) {
-            do {
-                var guess = prompt("Inserisci un numero da 1 a 100");
-            } while (!guess || isNaN(guess) || guess.trim() === "" || guess < 1 || guess > 100);
-            if (randomNumbers.includes(parseInt(guess))) {
-                matchingNumbers.push(parseInt(guess));
+        //Inserimento numeri utente
+        countdown.classList.add("hidden");
+        userGuesses.classList.remove("hidden");
+        send.addEventListener ("click", function() {
+            //Validation
+            var userGuess = guess.value;
+            if (!userGuess || isNaN(userGuess) || userGuess.trim() === "" || userGuess < 1 || userGuess > 100) {
+                alertMessage.classList.remove("hidden");
+                guess.value = "";
+            } else {
+                userNumbers.push(parseInt(userGuess));
+                alertMessage.classList.add("hidden");
+                guess.value = "";
             }
-        }
-        //Stampa risultato
-        if (matchingNumbers.length > 1) {
-            countdown.innerText = matchingNumbers.length + " numeri corretti: " + matchingNumbers.join(", ");
-        } else if (matchingNumbers.length === 1) {
-            countdown.innerText = "1 numero corretto: " + matchingNumbers;
-        } else {
-            countdown.innerText = "Nessun numero inserito è corretto";
-        }
+            //Verifica se il numero è corretto
+            if(userNumbers.length === 5) {
+                userGuesses.classList.add("hidden");
+                countdown.classList.remove("hidden");
+                for (let i = 0; i < 5; i++) {
+                    if (randomNumbers.includes(userNumbers[i]) && !matchingNumbers.includes(userNumbers[i])) {
+                        matchingNumbers.push(userNumbers[i]);
+                    }   
+                }
+                //Stampa risultato
+                if (matchingNumbers.length > 1) {
+                    countdown.innerText = matchingNumbers.length + " numeri corretti: " + matchingNumbers.join(", ");
+                } else if (matchingNumbers.length === 1) {
+                    countdown.innerText = "1 numero corretto: " + matchingNumbers;
+                } else {
+                    countdown.innerText = "Nessun numero inserito è corretto";
+                }
+            }
+        });
     } else {
-        countdown.innerText = --seconds;
+        countdown.innerText = seconds--;
     }
-}, 1000)
+}, 1000);
